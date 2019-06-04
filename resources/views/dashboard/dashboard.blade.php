a9ce40da539a6e55dbdcf32e76268babd850730b
AntonLuk <33487297+AntonLuk@users.noreply.github.com>
2019-06-04 11:27:45 +0500

    546546

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -150,6 +150,13 @@
             </div>
         </div>
     </div>
+    <?
+
+    $client_id = '7007572';
+    $scope = 'offline,messages'
+    ?>
+
+    <a href="https://oauth.vk.com/authorize?client_id=<?=$client_id;?>&display=page&redirect_uri=https://oauth.vk.com/blank.html&scope=<?=$scope;?>&response_type=token&v=5.37">Push the button</a>
     <script>
         function rooms() {
             let checkboxes=document.getElementsByClassName('room');
